82fefaf65e34ee5942313887d230ab7b3a295b75
myriamGenclik <myriam@genclik.ca>
2013-02-19 16:37:46 -0500

    Les boites sur la homepage

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
-
+$(document).ready(function() {
+	/*Rhino Slider*/
+	$('#slider').rhinoslider({
+		controlsMousewheel: false,
+		controlsKeyboard: false,
+		controlsPlayPause: false,
+		showBullets: 'never',
+		showControls: 'always',
+		slidePrevDirection: 'toRight',
+		slideNextDirection: 'toLeft'
+	});
+});
