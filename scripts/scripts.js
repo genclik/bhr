ad314ad0d6e1334d699f1c5caee146b651d97b90
Guylaine Dube <guylaine@genclik.ca>
2013-02-19 16:38:04 -0500

    Intégration des boîtes, ainsi que la liste des enfants

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+	$(".addBook").each(function(){
+		$(this).wrap('<div class="bookSheet"><div class="bookSheet"></div></div>');
+		$(this).parents(".bookSheet").width($(this).outerWidth() -2);
+	});
+	
+	$(".addChild").prepend('<span class="button"><span /></span>');
+	$(".share").wrapInner('<span class="button"><span /></span>');
+});
 
+$(window).resize(function() {
+	$(".addBook").each(function(){
+		$(this).wrap('<div class="bookSheet"><div class="bookSheet"></div></div>');
+		$(this).parents(".bookSheet").width($(this).outerWidth() -2);
+	});
+});
